13be03577b217a4d18efeecf08e1d3fa4a0096e8
rafay0606 <144208224+rafay0606@users.noreply.github.com>
2023-11-18 13:25:52 +0400

    Create Excercise2.py

---
+++ Chapter 7- Functions/Exercises/Excercise2.py
@@ -0,0 +1,8 @@
+#Excercise 2
+
+#Writing a function to display a favorite book
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('The Psychology of Money')
