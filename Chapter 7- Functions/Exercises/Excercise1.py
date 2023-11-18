060b52c385778ce9c869b198f39685b403e760b8
rafay0606 <144208224+rafay0606@users.noreply.github.com>
2023-11-18 13:24:30 +0400

    Create Excercise1.py

---
+++ Chapter 7- Functions/Exercises/Excercise1.py
@@ -0,0 +1,7 @@
+#Excercise 1
+
+# Creating a display message 
+def display_message():
+    print("We are studying the functions chapter. Functions are reusable blocks of code that perform a specific task, promoting modularity and code organization by encapsulating functionality." )
+
+display_message()
